fa2942d01717241763af49dcd3af854309a57b11
Mori Kouhei <morikouhe139@gmail.com>
2021-03-09 11:19:44 +0900

    upload new files

---
+++ atcoder/agc/agc052/a.py
@@ -0,0 +1,7 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    S = [input() for i in range(3)]
+    print("0"+"1"*n+"0"*n)
+
+    
---
+++ temp/MinCostFlow.py
@@ -0,0 +1,145 @@
+import sys
+input = sys.stdin.readline
+from heapq import heappush, heappop
+
+class mincostflow:
+
+    class edge:
+        def __init__(self, from_, to, cap, flow, cost):
+            self.from_ = from_
+            self.to = to
+            self.cap = cap
+            self.flow = flow
+            self.cost = cost
+    class _edge:
+        def __init__(self, to, rev, cap, cost):
+            self.to = to
+            self.rev = rev
+            self.cap = cap
+            self.cost = cost
+
+    def __init__(self, n):
+        self.n = n
+        self.pos = []
+        self.g = [[] for i in range(n)]
+
+    def add_edge(self, from_, to, cap, cost):
+        m = len(self.pos)
+        self.pos.append((from_, len(self.g[from_])))
+        self.g[from_].append(self.__class__._edge(to, len(self.g[to]), cap, cost))
+        self.g[to].append(self.__class__._edge(from_, len(self.g[from_])-1, 0, -cost))
+        return m
+
+    def get_edge(self, i):
+        _e = self.g[self.pos[i][0]][self.pos[i][1]]
+        _re = self.g[_e.to][_e.rev]
+        return self.edge(self.pos[i][0], _e.to, _e.cap + _re.cap, _re.cap, _e.cost)
+
+    def edges(self):
+        result = []
+        for i in range(len(self.pos)):
+            result.append(self.get_edge(i))
+        return result
+
+    def slope(self, s, t, flow_limit=10**20, inf=10**20):
+        dual = [0]*self.n
+        dist = [inf]*self.n
+        pv = [-1]*self.n
+        pe = [-1]*self.n
+        vis = [False]*self.n
+
+        def _dual_ref():
+            nonlocal dual, dist, pv, pe, vis
+            dist = [inf]*self.n
+            pv = [-1]*self.n
+            pe = [-1]*self.n
+            vis = [False]*self.n
+
+            que = [(0,s)]
+            dist[s] = 0
+            while que:
+                _,v = heappop(que)
+                if vis[v]:
+                    continue
+                vis[v] = True
+
+                if v == t:
+                    break
+                for i in range(len(self.g[v])):
+                    e = self.g[v][i]
+                    if vis[e.to] or e.cap == 0:
+                        continue
+                    cost = e.cost - dual[e.to] + dual[v]
+                    if dist[e.to] > dist[v] + cost:
+                        dist[e.to] = dist[v] + cost
+                        pv[e.to] = v
+                        pe[e.to] = i
+                        heappush(que, (dist[e.to],e.to))
+            if not vis[t]:
+                return False
+
+            for v in range(self.n):
+                if not vis[v]:
+                    continue
+                dual[v] -= dist[t] - dist[v]
+            return True
+        
+        flow = 0
+        cost = 0
+        prev_cost = -1
+
+        result = [(flow, cost)]
+        while flow < flow_limit:
+            if not _dual_ref():
+                break
+            c = flow_limit - flow
+            v = t
+            while v != s:
+                c = min(c, self.g[pv[v]][pe[v]].cap)
+                v = pv[v]
+
+            v = t
+            while v != s:
+                e = self.g[pv[v]][pe[v]]
+                e.cap -= c
+                self.g[v][e.rev].cap += c
+                v = pv[v]
+            
+            d = -dual[s]
+            flow += c
+            cost += c * d
+            if prev_cost == d:
+                result.pop()
+            result.append((flow, cost))
+            prev_cost = cost
+        return result
+
+    def flow(self, s, t, flow_limit=10**20):
+        return self.slope(s, t, flow_limit)[-1]
+    
+
+n,k = map(int,input().split())
+A = [list(map(int,input().split())) for i in range(n)]
+s = 2*n
+t = s+1
+g = mincostflow(t+1)
+
+M = 10**9
+g.add_edge(s,t,n*k,M)
+for i in range(n):
+    g.add_edge(s,i,k,0)
+    g.add_edge(n+i,t,k,0)
+
+for i in range(n):
+    for j in range(n):
+        g.add_edge(i,n+j,1,M-A[i][j])
+result = g.flow(s,t,n*k)
+print(n*k*M - result[1])
+edges = g.edges()
+ans = [["."]*n for i in range(n)]
+for e in edges:
+    if e.from_ == s or e.to == t or e.flow == 0:
+        continue
+    ans[e.from_][e.to - n] = "X"
+for i in ans:
+    print(*i, sep="")
