296c13c4002ee7ff40b2b596bf0d430bab45be61
morikouhei <morikouhe139@gmail.com>
2022-06-01 21:41:39 +0900

    upload new files

---
+++ atcoder/abc/abc253/e.py
@@ -0,0 +1,26 @@
+n,m,k = map(int,input().split())
+mod = 998244353
+
+if k == 0:
+    print(pow(m,n,mod))
+    exit()
+dp = [1]*m
+
+for i in range(n-1):
+    ndp = [0]*m
+    s = sum(dp)%mod
+    ndp[0] += s
+    for j in range(m):
+        x = dp[j]
+        ndp[max(0,j-k+1)] -= x
+        ndp[max(0,j-k+1)] %= mod
+        if j+k < m:
+            ndp[j+k] += x
+            ndp[j+k] %= mod
+
+    for j in range(m-1):
+        ndp[j+1] += ndp[j]
+        ndp[j+1] %= mod
+    dp = ndp
+    print(dp)
+print(sum(dp)%mod)
---
+++ atcoder/abc/abc253/g.py
@@ -0,0 +1,54 @@
+n,l,r = map(int,input().split())
+
+A = [i for i in range(n+1)]
+ans = []
+
+
+now = 0
+last = 0
+for i in range(1,n+1):
+    nnow = n-i + now
+    now = nnow
+    if l <= nnow:
+        
+        s = nnow-l
+        nnum = l
+        for j in range(n-s,n+1):
+            A[i],A[j] = A[j],A[i]
+            print(A)
+            nnum += 1
+            if nnum > r:
+                print(*A[1:])
+                exit()
+        for j in range(1,i+1):
+            ans.append(A[j])
+            A[j] = 0
+        last = i
+        print(i,s)
+        print(ans)
+        print(A)
+        break
+
+
+
+last += 1
+mi = 0
+for i in range(last,n+1):
+
+    nnow = n-i+now
+    print(i,nnow,ans)
+    print(A)
+    if nnow < r:
+        ans.append(A.pop())
+        mi += 1
+        now = nnow
+    else:
+        num = r-now
+        for j in range(num):
+            A[last],A[last+1+j] = A[last+1+j],A[last]
+
+        for j in A:
+            if j > 0:
+                ans.append(j)
+        break
+print(*ans)
